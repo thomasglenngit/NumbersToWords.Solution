49eb7993bbc0d8ac4ae8d85aca3676696d069582
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-16 16:07:02 -0600

    add passing test of Numbers 1 - 19 Dictionary.
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- NumbersToWords.Tests/ModelTests/Numbers.cs
+++ NumbersToWords.Tests/ModelTests/Numbers.cs
@@ -14,5 +14,15 @@ namespace NumbersToWords.Tests
       Numbers newNumbers = new Numbers();
       Assert.AreEqual(typeof(Numbers), newNumbers.GetType()); 
     }
+
+    [TestMethod]
+    public void GetNumbers19_MatchesNumberWithString_Dictionary()
+    {
+      Dictionary<int, string> testNumbers19 = new Dictionary<int, string>() { {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {11, "eleven"}, {12, "twelve"}, {13, "thirteen"}, {14, "fourteen"}, {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"}, {18, "eighteen"}, {19, "nineteen"} };
+      Numbers newNumbers = new Numbers();
+      Dictionary<int, string> result = newNumbers.Numbers19;
+      CollectionAssert.AreEqual(testNumbers19, result);
+
+    }
   }
 }
--- NumbersToWords/Models/Numbers.cs
+++ NumbersToWords/Models/Numbers.cs
@@ -4,11 +4,12 @@ namespace NumbersToWords.Models
 {
   public class Numbers
   {
-    public Dictionary<int, string> NumberString {get;}
-    // public Number()
-    // {
-
-    // }
+    public Dictionary<int, string> Numbers19 {get;}
+    public Numbers()
+    {
+      Numbers19 = new Dictionary<int, string>() { {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {11, "eleven"}, {12, "twelve"}, {13, "thirteen"}, {14, "fourteen"}, {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"}, {18, "eighteen"}, {19, "nineteen"} };
+      
+    }
     
   
   }
