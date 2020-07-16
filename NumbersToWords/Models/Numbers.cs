290daa337fdd65c24482ca84601c578f6d0b7a1b
Thomas Glenn <thomasgrantglenn@mac.com>
2020-07-16 17:03:48 -0600

    add last passing test of Numbers99 dictionary.
    Co-authored-by: Sara Kane <kane.sarak@gmail.com>

--- NumbersToWords/Models/Numbers.cs
+++ NumbersToWords/Models/Numbers.cs
@@ -6,30 +6,24 @@ namespace NumbersToWords.Models
   {
     public Dictionary<int, string> Numbers19 {get;}
     public Dictionary<int, string> Numbers99 {get;}
+    public Dictionary<int, string> Numbers999 {get;}
     public Numbers()
     {
       Numbers19 = new Dictionary<int, string>() { {1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"}, {7, "seven"}, {8, "eight"}, {9, "nine"}, {10, "ten"}, {11, "eleven"}, {12, "twelve"}, {13, "thirteen"}, {14, "fourteen"}, {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"}, {18, "eighteen"}, {19, "nineteen"} };
       
       Numbers99 = new Dictionary<int, string>() { {20, "twenty"}, {30, "thirty"}, {40, "forty"}, {50, "fifty"}, {60, "sixty"}, {70, "seventy"}, {80, "eighty"}, {90, "ninety"} };
 
+      Numbers999 = new Dictionary<int, string>() { {100, "hundred"};
+
     }
   }
 }
-/* 
-public class Car
-{
-    // Public properties
 
-    public string MakeModel { get; set; }
-    public int Price { get; set; }
-    public int Miles { get; set; }
-
-    // Constructor
-
-    public Car(string makeModel, int price, int miles)
-    {
-        MakeModel = makeModel;
-        Price = price;
-        Miles = miles;
-    }
-} */
+//public string NumbersToWords(int number)
+//if number <= 19
+// get value from Numbers 19
+//if number < 100
+//ones <- number % 10 
+//tens <- number - ones
+//get value from Numbers99 for tens
+//get value from Numbers19 for ones
